4429dff8dfd86c4a58ab074f3ce42172d8454274
Jin Geonwoo <jinkonu99@gmail.com>
2024-03-30 15:05:22 +0900

    feat: Java 스터디 내용 추가

--- firstSeminar/src/main/java/org/sopt/Main.java
+++ firstSeminar/src/main/java/org/sopt/Main.java
@@ -1,7 +1,19 @@
 package org.sopt;
 
+import org.sopt.classes.Person;
+import org.sopt.classes.PersonBuilder;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person person = Person.create("konu", 26, "male");
+        System.out.println("person.getName() = " + person.getName());
+        person.setName("abcde");
+        System.out.println("person.getName() = " + person.getName());
+
+        Person person2 = new PersonBuilder()
+                .name("konu")
+                .age(26)
+                .sex("male")
+                .build();
     }
 }
