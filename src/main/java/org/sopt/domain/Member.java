e259cdb9fa9c2f7f9a5e2960cd3e1d79ced52866
Jin Geonwoo <jinkonu99@gmail.com>
2024-04-06 15:22:23 +0900

    feat: Member 추가

---
+++ src/main/java/org/sopt/domain/Member.java
@@ -0,0 +1,26 @@
+package org.sopt.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Part part;
+
+    private int age;
+
+    public enum Part {
+        SERVER
+    }
+}
