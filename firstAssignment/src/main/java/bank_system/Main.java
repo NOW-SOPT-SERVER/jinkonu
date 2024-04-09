05a398c7baea00f3c85ce9f87ba1abe42a55b6fd
Jin Geonwoo <jinkonu99@gmail.com>
2024-04-09 17:53:08 +0900

    refactor: 메인 메서드 리팩토링

--- firstAssignment/src/main/java/bank_system/Main.java
+++ firstAssignment/src/main/java/bank_system/Main.java
@@ -1,7 +1,9 @@
 package bank_system;
 
+import bank_system.controller.BankController;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BankController.run();
     }
 }
