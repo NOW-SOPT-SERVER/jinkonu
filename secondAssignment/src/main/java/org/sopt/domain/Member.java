f93cbd10488e17dd5ec81e4134ec03f416b40177
Jin Geonwoo <jinkonu99@gmail.com>
2024-04-17 14:11:37 +0900

    refactor: Member에 도메인 로직 추가

--- secondAssignment/src/main/java/org/sopt/domain/Member.java
+++ secondAssignment/src/main/java/org/sopt/domain/Member.java
@@ -1,13 +1,17 @@
 package org.sopt.domain;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor
 public class Member {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -18,4 +22,24 @@ public class Member {
     private Part part;
 
     private int age;
+
+    private LocalDateTime createdAt;
+
+
+    public static Member create(String name, Part part, int age) {
+        return Member.builder()
+                .name(name)
+                .part(part)
+                .age(age)
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+
+    @Builder
+    public Member(int age, Part part, String name, LocalDateTime createdAt) {
+        this.age = age;
+        this.part = part;
+        this.name = name;
+        this.createdAt = createdAt;
+    }
 }
