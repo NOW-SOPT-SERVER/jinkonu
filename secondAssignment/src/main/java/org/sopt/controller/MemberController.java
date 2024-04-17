4b8d34bed5fd7bf344a3453820debdbe31ad27f7
Jin Geonwoo <jinkonu99@gmail.com>
2024-04-17 14:27:03 +0900

    feat: 컨트롤러 추가

---
+++ secondAssignment/src/main/java/org/sopt/controller/MemberController.java
@@ -0,0 +1,53 @@
+package org.sopt.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.sopt.dto.request.MemberCreateRequest;
+import org.sopt.dto.response.MemberGetAllResponse;
+import org.sopt.dto.response.MemberGetResponse;
+import org.sopt.service.MemberService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/member")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping
+    public ResponseEntity createMember(
+            @RequestBody MemberCreateRequest request
+    ) {
+        return ResponseEntity.created(URI.create(memberService.createMember(request)))
+                .build();
+    }
+
+    @GetMapping("/{memberId}")
+    public ResponseEntity<MemberGetResponse> findMemberById(
+            @PathVariable Long memberId
+    ) {
+        return ResponseEntity.ok(memberService.findMemberById(memberId));
+    }
+
+    @DeleteMapping("/{memberId}")
+    public ResponseEntity deleteMemberById(
+            @PathVariable Long memberId
+    ) {
+        memberService.deleteMemberById(memberId);
+
+        return ResponseEntity.noContent().build();
+    }
+
+
+    /*
+     * !구현과제!
+     * */
+    @GetMapping
+    public ResponseEntity<MemberGetAllResponse> findMembers() {
+        return ResponseEntity.ok(memberService.findMembers());
+    }
+}
