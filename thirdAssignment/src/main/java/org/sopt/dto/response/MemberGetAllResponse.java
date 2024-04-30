259fa4bd7a51906dbb1b0ce001180000cc83ab73
Jin Geonwoo <jinkonu99@gmail.com>
2024-04-30 17:41:34 +0900

    feat: 3rd assignment initial commit

---
+++ thirdAssignment/src/main/java/org/sopt/dto/response/MemberGetAllResponse.java
@@ -0,0 +1,15 @@
+package org.sopt.dto.response;
+
+import java.util.List;
+
+public record MemberGetAllResponse(
+        Long count,
+        List<MemberGetResponse> memberDtos
+) {
+    public static MemberGetAllResponse of(
+            long count,
+            List<MemberGetResponse> members
+    ) {
+        return new MemberGetAllResponse(count, members);
+    }
+}
