1bde5c57f7467a0b6c797bbadd8e31c58f700442
Jin Geonwoo <jinkonu99@gmail.com>
2024-04-30 17:44:17 +0900

    rename: 애플리케이션 클래스 이름 변경

--- thirdAssignment/src/main/java/org/sopt/thirdAssignmentApplication.java
+++ thirdAssignment/src/main/java/org/sopt/thirdAssignmentApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecondAssignmentApplication {
+public class thirdAssignmentApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SecondAssignmentApplication.class, args);
+		SpringApplication.run(thirdAssignmentApplication.class, args);
 	}
 
 }
